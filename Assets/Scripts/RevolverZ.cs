4af7deb9c2968dde2cfc1df2c40d45470486d049
Johann Prell <johann.prell@gmail.com>
2019-12-21 07:06:52 +0100

    * add revolving Torus x2

---
+++ Assets/Scripts/RevolverZ.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RevolverZ : MonoBehaviour
+{
+    public float Speed = 1f;
+    [SerializeField]
+    private float zRotation = 0f;
+    private Vector3 rotation;
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+        zRotation += Speed * Time.deltaTime;
+        if(zRotation >= 180f) zRotation = zRotation - 180f; // Keep the 'change' for smooth rotation
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            transform.rotation.eulerAngles.y,
+            zRotation);
+    }
+}
