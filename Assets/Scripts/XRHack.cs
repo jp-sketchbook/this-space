01cb1959043dd97221dcc9d4704571a1e553684e
Johann Prell <johann.prell@gmail.com>
2019-12-22 14:35:55 +0100

    * lift camera * make magical objects fade out very slowly
    Added hack for forced roomScale mode as well but doesn't seem to work

---
+++ Assets/Scripts/XRHack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+// Forces RoomScale XR Mode - known bug in Unity defaults back to stationary in various circumstances when loading
+public class XRHack : MonoBehaviour
+{
+    private bool _isRoomScale = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SetXRModeRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private IEnumerator SetXRModeRoutine() {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return true;
+            _isRoomScale =  XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+        }
+    }
+}
